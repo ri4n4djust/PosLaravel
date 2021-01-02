637ca1e39c592cd27faf8bd4d54584b06042d7c9
ri4n4djust <rian.adjust@gmail.com>
2021-01-02 11:46:44 +0800

    Update footerjs.blade.php

--- resources/views/includes/footerjs.blade.php
+++ resources/views/includes/footerjs.blade.php
@@ -21,59 +21,10 @@
 <!-- ChartJS -->
 <script src="{{asset('assets/bower_components/chart.js/Chart.js')}}"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{asset('assets/dist/js/pages/dashboard2.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="{{asset('assets/bower_components/ckeditor/ckeditor.js')}}"></script>
-<script src="{{asset('assets/dist/js/demo.js')}}"></script>
 <script src="{{asset('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js')}}"></script>
 
-<script>
-  $(function () {
-    // Replace the <textarea id="editor1"> with a CKEditor
-    // instance, using default configuration.
-    CKEDITOR.replace('editor1')
-    //bootstrap WYSIHTML5 - text editor
-    $('.textarea').wysihtml5()
-  })
-</script>
-<script>
-  $(function () {
-    // Replace the <textarea id="editor1"> with a CKEditor
-    // instance, using default configuration.
-    CKEDITOR.replace('des')
-    //bootstrap WYSIHTML5 - text editor
-    $('.textarea').wysihtml5()
-  })
-</script>
-<script>
-  $('#isuedatepassport').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-    })
-  $('#expdatepassport').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-    })
-</script>
 
-
-<script>
-  $('#flightDate').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-    })
-  $('#arrDate').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-    })
-</script>
-<script>
-  $('#joinDate').datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true
-    })
- 
-</script>
 <script src="{{asset('assets/bower_components/datatables.net/js/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
 
@@ -93,13 +44,4 @@
       'autoWidth'   : true
     })
   })
-</script>
-<script>
- // Basic example
-$(document).ready(function () {
-$('#dtBasicExample').DataTable({
-"ordering": false // false to disable sorting (or any other option)
-});
-$('.dataTables_length').addClass('bs-select');
-});
 </script>
