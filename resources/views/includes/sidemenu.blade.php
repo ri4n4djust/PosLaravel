5e56729ac5da6dd87a735dd66ce1216af5f54210
ri4n4djust <rian.adjust@gmail.com>
2021-01-02 11:46:50 +0800

    Update sidemenu.blade.php

--- resources/views/includes/sidemenu.blade.php
+++ resources/views/includes/sidemenu.blade.php
@@ -17,55 +17,40 @@
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
            </a>
         </li>
-         <li>
-          <a href="{{url('/adminslider')}}">
-            <i class="fa fa-calendar"></i> <span>Slider Home List</span>
+        <li class="treeview">
+          <a href="{{url('/barang')}}">
+            <i class="fa fa-calendar"></i> <span>Barang</span>
           </a>
         </li>
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-calendar"></i> <span>Applicant</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
+            <i class="fa fa-calendar"></i> <span>Pelanggan</span>
           </a>
-          <ul class="treeview-menu">
-            <li><a href="{{url('/TambahApplicant')}}"><i class="fa fa-circle-o"></i>Add Applicant</a></li>
-            <li><a href="{{url('/adminapplicant')}}"><i class="fa fa-circle-o"></i>Applicant List</a></li>
-            <li><a href="{{url('/yeslist')}}"><i class="fa fa-circle-o"></i>Applicant Approve</a></li>
-            <li><a href="{{url('/nolist')}}"><i class="fa fa-circle-o"></i>Applicant No Approve</a></li>
-            <li><a href="{{url('/pending')}}"><i class="fa fa-circle-o"></i>Applicant Pending</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-calendar"></i> <span>Repeater</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="{{url('/adminrepeater')}}"><i class="fa fa-calendar"></i> <span>Repeater List</span></a></li>
-            <li><a href="{{url('/repeateronprogres')}}"><i class="fa fa-calendar"></i> <span>Repeater Onprogres</span></a></li><li>
-            <li><a href="{{url('/repeateronboard')}}"><i class="fa fa-calendar"></i> <span>Repeater On Board</span></a></li><li>
-          </ul>
         </li>
         <li>
           <a href="{{url('/adminberita')}}">
-            <i class="fa fa-calendar"></i> <span>List News</span>
+            <i class="fa fa-calendar"></i> <span>Supplier</span>
+          </a>
+        </li>
+        <li>
+          <a href="{{url('/adminberita')}}">
+            <i class="fa fa-calendar"></i> <span>Sales</span>
           </a>
         </li>
       
         <li class="treeview">
           <a href="#">
-            <i class="fa fa-table"></i> <span>List Potition</span>
+            <i class="fa fa-table"></i> <span>Laporan</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{url('/department')}}"><i class="fa fa-circle-o"></i>Department</a></li>
-            <li><a href="{{url('/adminposisi')}}"><i class="fa fa-circle-o"></i>List Potition</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Laporan Penjualan</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Laporan Pembelian</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Laporan Stok Opname</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Laporan Operasional</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i>Laporan Tagihan</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -75,10 +60,6 @@
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
-          <ul class="treeview-menu">
-            <li><a href="{{url('jdlgaleri')}}"><i class="fa fa-circle-o"></i>Album Name</a></li>
-            <li><a href="{{url('/admingaleri')}}"><i class="fa fa-circle-o"></i>List Gallery</a></li>
-          </ul>
         </li>
         <li>
           <a href="{{url('/adminship')}}">
