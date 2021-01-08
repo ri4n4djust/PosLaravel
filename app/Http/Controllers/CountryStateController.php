18c5cc90a20039dcb748a8dc894b6e2387005e57
ri4n4djust <rian.adjust@gmail.com>
2021-01-08 11:50:54 +0800

    Update CountryStateController.php

--- app/Http/Controllers/CountryStateController.php
+++ app/Http/Controllers/CountryStateController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Country;
 use App\State;
+use App\Kategori;
 
 class CountryStateController extends Controller
 {
     //
     public function getCountries()
     {
-        $data = Country::get();
+        $data = Kategori::get();
         return response()->json($data);
     }
     /**
