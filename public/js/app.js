41ad0a26455b17fbf59e448e3966f1d9025b8917
ri4n4djust <rian.adjust@gmail.com>
2021-02-08 07:08:17 +0800

    new

--- public/js/app.js
+++ public/js/app.js
@@ -64116,8 +64116,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\WinMax\Documents\GitHub\PosLaravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\WinMax\Documents\GitHub\PosLaravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/max/Documents/Web/PosSale/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/max/Documents/Web/PosSale/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
